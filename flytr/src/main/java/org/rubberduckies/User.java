02f641286552c596b1250bb19e147fe169651e66
dagleaves <68761152+dagleaves@users.noreply.github.com>
2022-03-15 12:54:30 -0400

    Initial implementation

---
+++ flytr/src/main/java/org/rubberduckies/User.java
@@ -0,0 +1,24 @@
+package org.rubberduckies;
+
+public class User {
+    
+    private String username;
+    private String password;
+    private UserRole role;
+    private UserData data;
+    private UserPreferences preferences;
+    private ArrayList<Booking> cart;
+    private ArrayList<BookingReceipt> history;
+    private ArrayList<User> linkedAccounts;
+    private ArrayList<UserData> savedPeople;
+    private ArrayList<Dog> dogs;
+    private boolean isFrequentFlyer;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+
+        //TODO: default construct variables
+    }
+}

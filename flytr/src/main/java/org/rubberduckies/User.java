11f6b859f298422225980b43aa3236fd60ff9fe8
dagleaves <68761152+dagleaves@users.noreply.github.com>
2022-03-15 20:36:37 -0400

    Implement constructors

--- flytr/src/main/java/org/rubberduckies/User.java
+++ flytr/src/main/java/org/rubberduckies/User.java
@@ -13,12 +13,38 @@ public class User {
     private ArrayList<UserData> savedPeople;
     private ArrayList<Dog> dogs;
     private boolean isFrequentFlyer;
-
+    
     public User(String username, String password, UserRole role) {
         this.username = username;
         this.password = password;
         this.role = role;
+        this.data = new UserData();
+        this.preferences = new UserPreferences();
+        this.card = new ArrayList<Booking>();
+        this.history = new ArrayList<BookingReceipt>();
+        this.linkedAccounts = new ArrayList<User>();
+        this.savedPeople = new ArrayList<UserData>();
+        this.dogs = new ArrayList<Dogs>();
+        this.isFrequentFlyer = false;
+    }
 
-        //TODO: default construct variables
+    public User(String username, String password, UserRole role, UserData data, UserPreferences preferences, 
+            ArrayList<Booking> cart, ArrayList<BookingReceipt> history, ArrayList<User> linkedAccounts, 
+            ArrayList<UserData> savedPeople, ArrayList<Dog> dogs, boolean isFrequentFlyer) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.data = data;
+        this.preferences = new UserPreferences();
+        this.card = new ArrayList<Booking>();
+        this.history = new ArrayList<BookingReceipt>();
+        this.linkedAccounts = new ArrayList<User>();
+        this.savedPeople = new ArrayList<UserData>();
+        this.dogs = new ArrayList<Dogs>();
+        this.isFrequentFlyer = false;
+    }
+
+    public String getUsername() {
+        
     }
 }

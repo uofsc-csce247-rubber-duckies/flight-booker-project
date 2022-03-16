154022f58341c7e75bbb3236a66c1246a65bb760
joecomisk11 <70121407+joecomisk11@users.noreply.github.com>
2022-03-15 22:06:41 -0400

    Create Hotel.java

---
+++ flytr/src/main/java/org/rubberduckies/Hotel.java
@@ -0,0 +1,32 @@
+@author Joe
+
+public class Hotel {
+    private String name;
+    private Location location;
+    private ArrayList<HotelRoom> rooms;
+
+    public Hotel(String name, Location location)
+    {
+        return Hotel;
+    }
+    public boolean getRoom(int capacity)
+    {
+        return true;
+    }
+    public boolean bookRoom(int capacity)
+    {
+        return true;
+    }
+    public String toString()
+    {
+        return " ";
+    }
+    private void addRoom(int capacity)
+    {
+
+    }
+    private void removeRoom(int roomNumber)
+    {
+        
+    }
+}

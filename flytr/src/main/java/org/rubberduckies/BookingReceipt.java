3f509351161c1ae7602bad9c375f42c36cccde33
joecomisk11 <70121407+joecomisk11@users.noreply.github.com>
2022-03-15 21:58:56 -0400

    Create BookingReceipt.java

---
+++ flytr/src/main/java/org/rubberduckies/BookingReceipt.java
@@ -0,0 +1,51 @@
+@author Joe
+import java.time.LocalDateTime;
+
+public class BookingReceipt{
+    private Booking booking;
+    private User bookedBy;
+    private LocalDateTime bookedOn;
+    private ArrayList<UserData> users;
+
+    public BookingReceipt(Booking booking, User user)
+    {
+        return BookingReceipt;
+    }
+    public BookingReceipt(Booking booking, User user, UserData users)
+    {
+        return BookingReceipt;
+    }
+    public getBooking()
+    {
+        return booking;
+    }
+    public getBookedBy()
+    {
+        return bookedBy;
+    }
+    public getBookedOn()
+    {
+        return bookedOn;
+    }
+    public getUsers()
+    {
+        return users; 
+    }
+    public setBooking(Booking b)
+    {
+        this.booking = b;
+    }
+    public setBookedBy(User b)
+    {
+        this.bookedBy = b;
+    }
+    public setBookedOn(LocalDateTime b)
+    {
+        this.bookedOn = b;
+    }
+    public setUsers(ArrayList<UserData> b)
+    {
+        this.users = b;
+    }
+
+}
